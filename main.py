7737f4110cbde1f4eef32f9d4427e5e08e6c05c1
Nachiketa Jha <nachi>
2024-02-01 22:06:08 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, image = cap.read()
+    if not success:
+        break
+
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(imageRGB)
+
+    # Checking whether a hand is detected
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:  # Working with each hand
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = image.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+
+                if id == 20:
+                    cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+
+    cv2.imshow("Output", image)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
